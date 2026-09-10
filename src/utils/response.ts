3c613074808c9eb38c455b0a8cf90f54f387b4e5
surprise2024-cpu <surprise2024@gmail.com>
2026-09-10 13:25:10 +0200

    docs: enhance comments for clarity in response utility functions

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,21 +1,27 @@
+// ServerResponse represents the respons my server send back to the client.
 import { ServerResponse } from "http";
 
 export const sendSuccess = (
 
-    res: ServerResponse,
-    statusCode: number,
-    data: unknown
+    res: ServerResponse, 
+    statusCode: number, // http status code
+    data: unknown // data to end
 
 ): void => {
 
+    // sets information about the HTTP response
+    // sets status code first
     res.writeHead(statusCode, {
-        'content-type': 'application/json'
+        'content-type': 'application/json' // tells client that the data being sent back is JSON
     });
 
+    // sends response back to the client and ends the request
+    // once res.end() runs, the server is finished responding.
     res.end(
+        // JSON.stringify converts a javascript object into json text
         JSON.stringify({
-            success: true,
-            data
+            success: true, // tells client that the request worked
+            data // puts received data into response
         })
     );
 
@@ -25,7 +31,7 @@ export const sendError = (
 
     res: ServerResponse,
     statusCode: number,
-    message: string
+    message: string // error message you want to send
 
 ): void => {
 
